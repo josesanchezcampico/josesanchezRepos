91b33b7a54db99db1305687a4fdfa328856b37ec
Jose Sanchez <josesanchez@elcampico.org>
2019-03-13 20:33:59 +0100

    Esto es un cambio

--- Proyecto_ED/src/Holamundo.java
+++ Proyecto_ED/src/Holamundo.java
@@ -3,8 +3,8 @@ public class Holamundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("Hola mundo");
-	System.out.println("Hola a todos esto es una prueba");
+		System.out.println("Hola mundo");
+		System.out.println("Hola a todos");
 	}
 
 }
