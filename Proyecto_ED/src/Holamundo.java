8625ff6750ca93d30fe9f97cd675763591111fe5
Jose Sanchez <josesanchez@elcampico.org>
2019-03-13 20:32:31 +0100

    ESTA ES MI PRIMERA SUBIDA

---
+++ Proyecto_ED/src/Holamundo.java
@@ -0,0 +1,10 @@
+
+public class Holamundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("Hola mundo");
+	System.out.println("Hola a todos esto es una prueba");
+	}
+
+}
